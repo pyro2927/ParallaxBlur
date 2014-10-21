27b6eb432d1bed92d62840cfdf1ecc44484f14cc
Anas Bouzoubaa <anas.bouzoubaa@gmail.com>
2014-10-21 23:41:57 +0100

    Added interactions delegate with a method to catch when the header image is tapped

--- Pod/Classes/JPBParallaxBlurViewController.h
+++ Pod/Classes/JPBParallaxBlurViewController.h
@@ -8,11 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol JPBParallaxBlurInteractionsDelegate;
+
 @interface JPBParallaxBlurViewController : UIViewController
 
 - (void)setHeaderImage:(UIImage*)headerImage;
 - (void)addHeaderOverlayView:(UIView*)overlay;
 - (CGFloat)headerHeight;
 - (UIScrollView*)mainScrollView;
+
+@property (weak, nonatomic, readwrite) id <JPBParallaxBlurInteractionsDelegate> interactionsDelegate;
+
+@end
+
+///-------------------------------------------------------------------------------------------------------
+/// Interactions Delegate
+///-------------------------------------------------------------------------------------------------------
+
+@protocol JPBParallaxBlurInteractionsDelegate <NSObject>
+
+@optional
+
+/**
+ Called when the header imageview is tapped.
+ */
+- (void)didTapHeaderImageView:(UIImageView*)imageView;
 
 @end
